09ce551fe7ffd816783765da2cb1a20b5c7530e9
Raffaello Bonghi <raffaello@rnext.it>
2019-12-26 20:16:56 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,121 @@
+# -*- coding: UTF-8 -*-
+# This file is part of the orbot package (https://github.com/officinerobotiche/orbot or http://www.officinerobotiche.it).
+# Copyright (C) 2020, Raffaello Bonghi <raffaello@rnext.it>
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its 
+#    contributors may be used to endorse or promote products derived 
+#    from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Reference:
+# 1. https://packaging.python.org
+# 2. https://julien.danjou.info/starting-your-first-python-project/
+# 3. https://medium.com/@trstringer/the-easy-and-nice-way-to-do-cli-apps-in-python-5d9964dc950d
+# 4. https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-and-scripts/
+# 5. https://python-packaging.readthedocs.io
+# 6. https://github.com/pypa/sampleproject
+# 7. https://pypi.org/classifiers/
+# 8. https://stackoverflow.com/questions/20288711/post-install-script-with-python-setuptools
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+# Launch command
+import subprocess as sp
+import shlex
+import os
+import sys
+import re
+
+
+
+here = path.abspath(path.dirname(__file__))
+project_homepage = "https://github.com/officinerobotiche/orbot"
+
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Load version package
+with open(os.path.join(here, "orbot", "__init__.py")) as fp:
+    VERSION = (
+        re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S).match(fp.read()).group(1)
+    )
+# Store version package
+version = VERSION
+
+
+# Configuration setup module
+setup(
+    name="orbot",
+    version=version,
+    author="Raffaello Bonghi",
+    author_email="raffaello.bonghi@officinerobotiche.it",
+    description="Telegram Officine robotiche bot",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=project_homepage,
+    download_url=(project_homepage + "/archive/master.zip"),
+    project_urls={
+        "How To": (project_homepage + "/tree/master/docs"),
+        "Examples": (project_homepage + "/tree/master/examples"),
+        "Bug Reports": (project_homepage + "/issues"),
+        "Source": (project_homepage + "/tree/master")
+    },
+    packages=find_packages(exclude=['examples', 'scripts', 'tests']),  # Required
+    keywords=("telegram bot calendar manager"
+              ),
+    classifiers=["Development Status :: 4 - Beta",
+                 # Audiencence and topics
+                 "Intended Audience :: Developers",
+                 # License
+                 "License :: OSI Approved :: MIT License",
+                 # Programming and Operative system
+                 "Programming Language :: Python :: 2",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 3.4",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
+                 "Programming Language :: Python :: 3.7",
+                 "Operating System :: POSIX :: Linux",
+                 ],
+    # Requisites
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    platforms=["linux", "linux2", "darwin"],
+    # Zip safe configuration
+    # https://setuptools.readthedocs.io/en/latest/setuptools.html#setting-the-zip-safe-flag
+    zip_safe=False,
+    # The following provide a command called `orbot`
+    entry_points={'console_scripts': ['orbot=orbot.__main__:main']},
+)
+# EOF
