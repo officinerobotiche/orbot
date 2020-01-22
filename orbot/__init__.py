5195952938dfaafed93dbc65211d307a7e519426
Raffaello Bonghi <raffaello@rnext.it>
2020-01-22 23:25:25 +0000

    Fix version

--- orbot/__init__.py
+++ orbot/__init__.py
@@ -34,5 +34,5 @@ from .orbot import ORbot
 from .orbot import get_version
 
 __author__ = "Raffaello Bonghi <raffaello.bonghi@officinerobotiche.it>"
-__version__ = "0.1.6"
+__version__ = "0.1.8"
 # EOF
